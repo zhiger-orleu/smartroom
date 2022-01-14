33e7202842abc22b878111d51d80acaa94d26ffd
zhiger-orleu <97661647+zhiger-orleu@users.noreply.github.com>
2022-01-14 19:27:38 +0600

    Add files via upload

---
+++ js/message.js
@@ -0,0 +1,12 @@
+function send_handle() {
+	
+	let name = document.getElementById("form-name").value + document.getElementById("popup_form-city").value;
+	
+	let phone = document.getElementById("form-phone").value + document.getElementById("popup_form-phone").value;
+
+	let city = document.getElementById("form-city").value + document.getElementById("popup_form-city").value;
+
+	var win = window.open(`https://wa.me/77470941770?text=Здравствуйте, меня зовут ${name}, я из города ${city}, прошу связаться со мной по номеру телефона - ${phone}`, '_blank');
+
+	window.location.reload();
+}
