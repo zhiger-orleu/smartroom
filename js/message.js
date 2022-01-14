893e8f48ff7ca3239508cacd7ea731bbf8e52732
zhiger-orleu <97661647+zhiger-orleu@users.noreply.github.com>
2022-01-14 19:37:35 +0600

    Update message.js

--- js/message.js
+++ js/message.js
@@ -5,8 +5,10 @@ function send_handle() {
 	let phone = document.getElementById("form-phone").value + document.getElementById("popup_form-phone").value;
 
 	let city = document.getElementById("form-city").value + document.getElementById("popup_form-city").value;
-
-	var win = window.open(`https://wa.me/77470941770?text=Здравствуйте, меня зовут ${name}, я из города ${city}, прошу связаться со мной по номеру телефона - ${phone}`, '_blank');
-
+	
+	if (name !== "" && phone !== "" && city !== "") {
+		var win = window.open(`https://wa.me/77470941770?text=Здравствуйте, меня зовут ${name}, я из города ${city}, прошу связаться со мной по номеру телефона - ${phone}`, '_blank');
+	}
+	
 	window.location.reload();
 }
