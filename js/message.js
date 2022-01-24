06cddc754120af48122fa15d918ea706c225df23
zhiger-orleu <97661647+zhiger-orleu@users.noreply.github.com>
2022-01-24 19:19:33 +0600

    Update message.js

--- js/message.js
+++ js/message.js
@@ -1,10 +1,20 @@
-function send_handle() {
+function send() {
 	
-	let name = document.getElementById("form-name").value + document.getElementById("popup_form-city").value;
+	let name = document.getElementById("form-name").value;
+	let phone = document.getElementById("form-phone").value;
+	let city = document.getElementById("form-city").value;
 	
-	let phone = document.getElementById("form-phone").value + document.getElementById("popup_form-phone").value;
+	if (name !== "" && phone !== "" && city !== "") {
+		var win = window.open(`https://wa.me/77470941770?text=Здравствуйте, меня зовут ${name}, я из города ${city}, прошу связаться со мной по номеру телефона - ${phone}`, '_blank');
+		window.location.reload();
+	}
+}
 
-	let city = document.getElementById("form-city").value + document.getElementById("popup_form-city").value;
+function send_popup() {
+	
+	let name = document.getElementById("popup_form-city").value;
+	let phone = document.getElementById("popup_form-phone").value;
+	let city = document.getElementById("popup_form-city").value;
 	
 	if (name !== "" && phone !== "" && city !== "") {
 		var win = window.open(`https://wa.me/77470941770?text=Здравствуйте, меня зовут ${name}, я из города ${city}, прошу связаться со мной по номеру телефона - ${phone}`, '_blank');
